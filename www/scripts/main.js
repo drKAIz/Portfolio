1b1422a1a52d39a134445847eefc0948cbf908b0
K.A.I <32905161+drKAIz@users.noreply.github.com>
2019-03-14 14:36:25 -0300

    Demo website
    Featuring mostly fictional information.

---
+++ www/scripts/main.js
@@ -0,0 +1,10 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'drKAI';
+
+var myVar;
+myVar = 'His Awesomeness';
+if (myVar === 'His Awesomeness') {
+  alert('That\'s right!');
+} else {
+  alert('Nah...');
+}
